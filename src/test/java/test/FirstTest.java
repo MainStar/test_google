f394448f1020c2d77ee55c4016f6d444089f97b7
PetroKiryshkin <petr.kiryshkin@gmail.com>
2018-03-15 10:55:36 +0200

    Create Test on check title on Java Letter.

--- src/test/java/test/FirstTest.java
+++ src/test/java/test/FirstTest.java
@@ -9,19 +9,31 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class FirstTest {
 
     private static WebDriver driver;
+    private List<WebElement> webElements = new ArrayList<WebElement>();
+    private List<String> urlsList = new ArrayList<String>();
 
     @BeforeClass
-    public static void connectToBrowser(){
+    public static void setUp(){
         System.setProperty("webdriver.gecko.driver", "C:/Users/user/Desktop/geckodriver.exe");
         driver = new FirefoxDriver();
     }
 
+    @AfterClass
+    public static void tearDown(){
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
     @Test
-    public void testGoogleSearch(){
+    public void testGoogleSearch() {
         driver.get("https://www.google.com/");
         Assert.assertEquals("Google", driver.getTitle());
         WebElement searchField = driver.findElement(By.id("lst-ib"));
@@ -29,12 +41,37 @@ public class FirstTest {
         WebElement buttonSearch = driver.findElement(By.name("btnK"));
         buttonSearch.click();
         Assert.assertEquals("java - Поиск в Google", driver.getTitle());
+
+        /** Ввытаскиваем все ссылки с каждого поиска гугл */
+        webElements = (driver.findElements(By.className("iUh30")));
+        System.out.println(webElements.size());
+
+        /** Выводим все урлы */
+        for (int i = 0; i < webElements.size(); i++) {
+            urlsList.add(webElements.get(i).getText());
+            System.out.println(webElements.get(i).getText());
+        }
+
+        for (int i = 0; i < urlsList.size(); i++){
+            String url = urlsList.get(i);
+            driver.get(url);
+            System.out.println("\n"+url);
+            String title = driver.getTitle();
+            System.out.println(title);
+            checkAllTitles(title);
+        }
     }
 
-    @AfterClass
-    public static void closeBrowser(){
-        if (driver != null) {
-            driver.quit();
+    private static void checkAllTitles(String title){
+
+        String [] massTitle;
+        massTitle = title.split(" ");
+        for (int i = 0; i < massTitle.length; i++){
+            if (massTitle[i].equals("Java") || massTitle[i].equals("java")){
+                Assert.assertEquals("Java", massTitle[i]);
+            }else {
+                Assert.assertNotEquals("Java", massTitle[i]);
+            }
         }
     }
 }
